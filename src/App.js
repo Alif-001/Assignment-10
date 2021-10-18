6de79c802730361f170718aba0652d7f5cf251b2
MD. OMOR FARUK ALIF <alifh8282@gmali.com>
2021-10-18 14:34:05 +0600

    template created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import About from "./components/Pages/About/About";
+import Contact from "./components/Pages/Contact/Contact";
+import Home from "./components/Pages/Home/Home";
+import Login from "./components/Pages/Login/Login";
+import Regiseter from "./components/Pages/Register/Regiseter";
+import Services from "./components/Pages/Services/Services";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <BrowserRouter>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/services">
+            <Services></Services>
+          </Route>
+          <Route path="/about">
+            <About></About>
+          </Route>
+          <Route path="/contact">
+            <Contact></Contact>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
+          </Route>
+          <Route path="/register">
+            <Regiseter></Regiseter>
+          </Route>
+          <Route></Route>
+          <Route></Route>
+        </Switch>
+        <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMedkit } from "@fortawesome/free-solid-svg-icons";
+import { faMedkit, faUser } from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   const element = <FontAwesomeIcon icon={faMedkit} />;
+  const element2 = <FontAwesomeIcon icon={faUser} />;
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={NavLink} to="/home">
           <span className=" fa-2x">{element} </span>
-          <span className="fs-2">Health Ninja </span>
+          <span className="fs-2 fw-bold">Health Ninja </span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="ms-auto">
+          <Nav className="ms-auto fw-bold fs-5 menu">
             <Nav.Link as={NavLink} to="/home">
               Home
             </Nav.Link>
@@ -24,23 +27,36 @@ const Header = () => {
             <Nav.Link as={NavLink} to="/about">
               About
             </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact">
+              Contact
+            </Nav.Link>
             <Nav.Link as={NavLink} to="/login">
               Login
             </Nav.Link>
             <Nav.Link as={NavLink} to="/Register">
               Register
             </Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            {/* <NavDropdown
+              title={
+                <img
+                  style={{
+                    width: "45px",
+                    borderRadius: "50%",
+                  }}
+                  src={photoURL} 
+                  alt={element2}
+                />
+              }
+              id="collasible-nav-dropdown"
+            >
+              <div className="text-center">
+                <h6>name</h6>
+                <p className="m-0 mb-2"></p>
+                <button onClick={logOut} className="btn btn-primary">
+                  Sign Out
+                </button>
+              </div>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
